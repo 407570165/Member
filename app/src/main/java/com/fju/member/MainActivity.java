be993cc8795db11729f76f07bea73dd2ebc055e1
miia <miia@com.tw>
2020-01-07 14:06:52 +0800

    1st

--- app/src/main/java/com/fju/member/MainActivity.java
+++ app/src/main/java/com/fju/member/MainActivity.java
@@ -2,13 +2,26 @@ package com.fju.member;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editName;
+    boolean isTrue =false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editName = findViewById(R.id.edit_name);
+        String a =editName.getText().toString();
+        if (a.equals(""))
+            isTrue=true;
+        if (isTrue){
+            Intent intent =new Intent(this,NicknameActivity.class);
+            startActivity(intent);
+        }
     }
+
 }
