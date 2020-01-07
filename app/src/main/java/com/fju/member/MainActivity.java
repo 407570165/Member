92873dc0700811276248aca3ea898bc8dd69b310
miia <miia@com.tw>
2020-01-07 14:35:28 +0800

    first

--- app/src/main/java/com/fju/member/MainActivity.java
+++ app/src/main/java/com/fju/member/MainActivity.java
@@ -1,5 +1,6 @@
 package com.fju.member;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,18 +11,33 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText editName;
     boolean isTrue =false;
+    private String a;
+    int b=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editName = findViewById(R.id.edit_name);
-        String a =editName.getText().toString();
-        if (a.equals(""))
-            isTrue=true;
-        if (isTrue){
-            Intent intent =new Intent(this,NicknameActivity.class);
-            startActivity(intent);
+        a = editName.getText().toString();
+        if (b==1) {
+            Intent intent = new Intent(this, NicknameActivity.class);
+            startActivityForResult(intent, 10);
+            b++;
         }
     }
 
-}
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==10){
+            if (resultCode!=RESULT_OK){
+                finish();
+            }else{
+                String  name =getSharedPreferences("text",MODE_PRIVATE)
+                        .getString("nameId","");
+                editName.setText(name);
+            }
+            }
+        }
+    }
+
--- app/src/main/java/com/fju/member/NicknameActivity.java
+++ app/src/main/java/com/fju/member/NicknameActivity.java
@@ -2,14 +2,38 @@ package com.fju.member;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class NicknameActivity extends AppCompatActivity {
+public class NicknameActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText editText;
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nickname);
+        editText = findViewById(R.id.nickname);
+        name = editText.getText().toString();
+        findViewById(R.id.imageView).setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case (R.id.imageView):
+            name = editText.getText().toString();
+            SharedPreferences setting = getSharedPreferences("texr", MODE_PRIVATE);
+            setting.edit()
+                    .putString("nameId", name)
+                    .commit();
+            setResult(RESULT_OK);
+            finish();
+            break;
+        }
     }
 }
